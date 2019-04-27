5a079ae30b08d929bdb7b18ca64dfb6ee4b5bcfa
jonesnxt <jones25519@gmail.com>
2019-04-26 20:12:13 -0700

    gem match start

--- frontend/pages/HomePage/index.js
+++ frontend/pages/HomePage/index.js
@@ -1,78 +1,159 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled, { ThemeProvider, keyframes } from 'styled-components';
+import GlobalContext from 'GlobalContext';
+import { Helmet } from 'react-helmet';
+import { FaVolumeUp, FaVolumeMute } from 'react-icons/fa';
 
-import GlobalContext from '../../common/GlobalContext';
-import Request from '../../koji_utilities/request';
-import Routes from '../../koji_utilities/routes';
+import Game from './components/Game';
 
 const Container = styled.div`
-    background-color: ${({ theme }) => theme.colors.backgroundColor};
-    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background: url(${({ theme }) => theme.general.backgroundImage});
+    background-size: cover;
+    background-position: center;
+    flex-direction: column;
+    text-align:center;
+`;
+
+function getFontFamily(ff) {
+    const start = ff.indexOf('family=');
+    if(start === -1) return 'sans-serif';
+    let end = ff.indexOf('&', start);
+    if(end === -1) end = undefined;
+    return ff.slice(start + 7, end);
+}
+
+const Cover = styled.div`
+    transition: opacity 0.7s ease-in-out;
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: linear-gradient(transparent 0, ${({ color }) => color});
+    opacity: ${({ colorSwitch }) => colorSwitch ? 1 : 0};
+`;
+
+const Content = styled.div`
+    width: 100%;
+    height: 100vh;
+    opacity: 1;
+    z-index: 1;
+    color: ${({ theme }) => theme.style.textColor};
+    text-shadow: 0 1px 6px rgba(0,0,0,0.7);
+    font-family: '${({ theme }) => getFontFamily(theme.general.fontFamily)}', sans-serif;
+    position: relative;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: ${({ theme }) => theme.colors.textColor};
-    text-align: center;
 `;
 
-const AppLogoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
+const VolumeControl = styled.div`
+    position: absolute;
+    top: 6px;
+    right: 6px;
+    font-size: 14px;
+    background-color: rgba(0,0,0,0.6);
+    border-radius: 6px;
+    padding: 6px;
+    display: flex;
+    align-items: center;
+    color: white;
+
+    &:hover {
+        background-color: rgba(0,0,0,0.9);
+    }
 `;
 
-const Content = styled.div`
-  padding-bottom: 8px;
+const Title = styled.h1`
+    font-size: 32px;
+    margin: 0;
+    margin-bottom: 12px;
 `;
 
-const Link = styled.a`
-  color: ${({ theme }) => theme.colors.linkColor};
+const Animation = keyframes`
+    0% {
+        transform: scale(1,1);
+    }
+    50% {
+        transform: scale(0.85,0.85);
+    }
+    100% {
+        transform: scale(1,1);
+    }
 `;
 
-const Icon = styled.div`
-    animation: ${AppLogoSpin} infinite 20s linear;
-    height: 40vmin;
-    width: 60vmin;
-    pointer-events: none;
-    background-image: url(${({ theme }) => theme.images.icon});
-    background-size: cover;
-    margin-bottom: 16px;
+const Start = styled.button`
+    max-width: 320px;
+    font-size: 24px;
+    background: rgba(0,0,0,0.7);
+    border: 4px solid ${({ theme }) => theme.style.textColor};
+    color: ${({ theme }) => theme.style.textColor};
+    box-shadow: 0 2px 12px rgba(0,0,0,0.24);
+    text-shadow: 0 1px 6px rgba(0,0,0,0.4);
+    cursor: pointer;
+    padding: 10px 64px;
+    border-radius: 1000px;
+    margin-bottom: 10vh;
+    animation: ${Animation} 2s ease-in-out infinite;
+
+    transition: background-color 0.1s ease-in-out;
+
+    &:hover {
+        background-color: rgba(0,0,0,0.3);
+    }
 `;
 
-class HomePage extends React.PureComponent {
+class HomePage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            response: '',
+            color: '#fff',
+            start: false,
+            muted: localStorage.getItem('muted') === 'true' || false,
         };
     }
 
-    componentDidMount() {
-        Request(Routes.SampleRoute).then((e) => this.setState({ response: e.response }));
-    }
+  flash(color) {
+    this.setState({ color, colorSwitch: true });
+    setTimeout(() => this.setState({ colorSwitch: false }), 700);
+  }
 
-    render() {
-        return (
-            <Container>
-                <Icon />
-                <Content>{this.state.response}</Content>
-                <Content>{this.context.strings.content}</Content>
-                <Link
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                    Learn React
-                </Link>
-            </Container>
-        );
-    }
+  toggleMute(muted) {
+      this.setState({ muted });
+      localStorage.setItem('muted', muted);
+  }
+
+  render() {
+    return (
+        <Container color={this.state.color}>
+            <Helmet defaultTitle={this.context.general.name}>
+                <link href={this.context.general.fontFamily} rel="stylesheet" />
+                <link rel="icon" href={this.context.metadata.icon} sizes="32x32" />
+            </Helmet>
+            <Cover color={this.state.color} colorSwitch={this.state.colorSwitch} />
+            <Content>
+                <VolumeControl>
+                    {this.state.muted ? 
+                        <FaVolumeMute onClick={() => this.toggleMute(false)} />
+                      : <FaVolumeUp onClick={() => this.toggleMute(true)} />
+                    }
+                </VolumeControl>
+                <Title>{this.context.general.name}</Title>
+                {this.state.start ? (
+                    <Game onFlash={(color) => this.flash(color)} muted={this.state.muted} />
+                ) : (
+                    <Start onClick={() => this.setState({ start: true })}>{this.context.general.buttonText}</Start>
+                )}
+            </Content>
+        </Container>
+    );
+  }
 }
 
 HomePage.contextType = GlobalContext;
